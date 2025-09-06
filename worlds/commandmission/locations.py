af55faf7a8953ffacb4e8e6c1f780d65f193fc2b
themogulAP <themogulow@gmail.com>
2025-09-06 00:48:25 -0500

    Update locations.py
    updated all of Lagrano

--- worlds/commandmission/locations.py
+++ worlds/commandmission/locations.py
@@ -1,4 +1,4 @@
-from tpying import NamedTuple, Dict, Optional
+from typing import NamedTuple, Dict, Optional, Callable
 from .Items import MMXCMItemData
 from .Helpers import MMXCMRamData
 
@@ -8,8 +8,144 @@ class MMXCMLocationData(NamedTuple):
   code:Optional[int]
   parent_region: str
   ram_addr: Optional[MMXCMRamData] = None
+  access_rule: Callable[[int], bool] = lambda state: True
 
 # This begins the list for every single randomized item in AP.
 LAGRANO_RUINS_LOCATIONS: dict[str, MMXCMLocationData] = {
   # Here is an example of the location and the bit position to track it.
-  "Area 1F East 50z"
+  "Area 1F East MD 1": MMXCMLocationData(
+    name="Area 1F East 1",
+    code=1,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x804A2153, bit_position=2),
+    access_rule=lambda state: state.has("Lagrano Access Code", 1)
+  ),
+  "Area 1F East MD 2": MMXCMLocationData(
+    name="Area 1F East 2", 
+    code=2,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x804A2153, bit_position=3)
+  ),
+  "Area 2F East MD 1": MMXCMLocationData(
+    name="Area 2F East 1",
+    code=3,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x804A2153, bit_position=5)
+  ),
+  "Area 2F East MD 2": MMXCMLocationData(
+    name="Area 2F East 2",
+    code=4,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x804A2153, bit_position=6)
+  ),
+  "Area 2F East MD 3": MMXCMLocationData(
+    name="Area 2F East 3",
+    code=5,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x804A2153, bit_position=7)
+  ),
+  "Area 2F East MD 4": MMXCMLocationData(
+    name="Area 2F East 4",
+    code=6,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042152, bit_position=0)
+  ),
+  "Area 3F East MD 1": MMXCMLocationData(
+    name="Area 3F East 1",
+    code=7,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRAMData(0x8042152, bit_position=3)
+  ),
+  "Area 3F East MD 2": MMXCMLocationData(
+    name="Area 3F East 2",
+    code=8,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042152, bit_position=2)
+  ),
+  "East Area Stairs 3F to 4F MD 1": MMXCMLocationData(
+    name="East Area Stairs 3F to 4F 1",
+    code=9,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042152, bit_position=4)
+  ),
+  "Area 4F East MD 1", MMXCMLocationData(
+    name="Area 4F East 1",
+    code=10,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=3)
+  ),
+  "Area 4F East MD 2", MMXCMLocationData(
+    name="Area 4F East 2",
+    code=11,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=2)
+  ),
+  "Area 4F East MD 3", MMXCMLocationData(
+    name="Area 4F East 3",
+    code=12,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=1)
+  ),
+  "4F: Test Hall 1 MD 1", MMXCMLocationData(
+  # These two entries are the MD's after Hippo is defeated.
+    name="4F Test Hall 1-1",
+    code=13,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=0)
+    access_rule=lambda state: state.has("Hippopressor Defeated", 1)
+  ),
+  "4F: Test Hall 1 MD 2", MMXCMLocationData(
+    name="4F Test Hall 1-2",
+    code=14,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042152, bit_position=7)
+    access_rule=lambda state: state.has("Hippopressor Defeated", 1)
+  ),
+  "Area 4F West MD 1", MMXCMLocationData(
+    name="Area 4F West 1",
+    code=15,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042152, bit_position=6)
+  ),
+  "Area 4F West MD 2", MMXCMLocationData(
+    name="Area 4F West 2",
+    code=16,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042152, bit_position=5)
+  ), 
+  "East Area Stairs 4F to 5F MD 1", MMXCMLocationData(
+    name="East Area Stairs 4F to 5F 1",
+    code=17,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=5)
+    access_rule=lambda state: state.has("Lagrano Key", 1)
+  ),
+  "East Area Stairs 4F to 5F MD 2", MMXCMLocationData(
+    name="East Area Stairs 4F to 5F 2",
+    code=18,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=6),
+    access_rule=lambda state: state.has("Lagrano Key, 1)
+  ),      
+  "East Area Stairs 4F to 5F MD 3", MMXCMLocationData(
+    name="East Area Stairs 4F to 5F 3",
+    code=19,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042151, bit_position=7),
+    access_rule=lambda state: state.has("Lagrano Key, 1)
+  ),
+  "Area 5F West MD 1", MMXCMLocationData(
+    name="Area 5F West 1",
+    code=20,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042150, bit_position=0)
+  ),
+  "Area 5F West MD 2", MMXCMLocationData(
+    name="Area 5F West 2",
+    code=21,
+    parent_region="Lagrano Ruins",
+    ram_addr=MMXCMRamData(0x8042150, bit_position=1)
+  ), 
+}
+
+    
