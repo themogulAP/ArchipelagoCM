26d9d1a8b200ded8cbb85091b4e7e6d85fb1680e
themogulAP <themogulow@gmail.com>
2025-09-05 23:32:35 -0500

    Update locations.py

--- worlds/commandmission/locations.py
+++ worlds/commandmission/locations.py
@@ -1,2 +1,15 @@
 from tpying import NamedTuple, Dict, Optional
 from .Items import MMXCMItemData
+from .Helpers import MMXCMRamData
+
+# This will list the exact bluepring for makign our locations! 
+class MMXCMLocationData(NamedTuple):
+  name:str
+  code:Optional[int]
+  parent_region: str
+  ram_addr: Optional[MMXCMRamData] = None
+
+# This begins the list for every single randomized item in AP.
+LAGRANO_RUINS_LOCATIONS: dict[str, MMXCMLocationData] = {
+  # Here is an example of the location and the bit position to track it.
+  "Area 1F East 50z"
