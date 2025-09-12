5ea8123c742d12f19e9e94e0cbd30c98592fa83d
themogulAP <themogulow@gmail.com>
2025-09-11 23:53:22 -0500

    Update __init__.py
    added descriptors

--- worlds/commandmission/__init__.py
+++ worlds/commandmission/__init__.py
@@ -21,26 +21,35 @@ class MMXCMWorld(World):
   topology_present: ClassVar[bool] = False
   data_version: ClassVar[int] = 1
 
+#Create the dictionaries that will map every item and every location for our AP! 
   item_name_to_id: ClassVar[dict[str, int]] = {name: data.ap_id for name, data in ALL_ITEMS_TABLE.items()}
   location_name_to_id: ClassVar[dict[str, int]] = {name: data.ap_id for name, data in LOCATION_TABLE.items()}
 
+#This is the very first piece of code that runs when a new MMX CM World will be created! 
+  #It will then map all of our items and locations to their respective Strings to IDs that we put in items and locations py. 
   def __init__(self, multiworld, player):
     super().__init__(multiworld, player)
     self.location_name_to_id = LOCATION_TABLE.ap_ids
     self.item_name_to_id = ALL_ITEMS_TABLE.ap_ids
 
+# This places any logic we need to before the generation process.
   def generate_early(self): 
     pass
 
+# This will build the entire map for our randomized AP! 
   def create_regions(self):
     pass
 
+# This will build the entire item pool for our randomized AP! 
   def create_items(self):
     pass
 
+# This will apply all the logic that we described in rules py! 
   def set_rules(self):
       set_rules(self)
 
+# This will bridge the game between the server that generates the seed and the client! 
+  # Example: this will tell the patcher to fix the encounter rate! 
   def fill_slot_data(self):
       return {}
     
