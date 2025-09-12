6c1f1a04b0551c7d89e4e3c8635f9e16fd49ca79
themogulAP <themogulow@gmail.com>
2025-09-12 01:51:33 -0500

    Update helpers.py
    added MMXCMRamData

--- worlds/commandmission/helpers.py
+++ worlds/commandmission/helpers.py
@@ -1,1 +1,9 @@
+from typing import NamedTuple, Optional 
 
+class MMXCMRamData(NamedTuple):
+    ram_addr: Optional[int] = None
+    bit_position: Optional[int] = None
+    ram_byte_size: Optional[int] = None
+    pointer_offset: Optional[int] = None
+    in_game_room_id: Optional[int] = None
+    item_count: Optional[int] = None
