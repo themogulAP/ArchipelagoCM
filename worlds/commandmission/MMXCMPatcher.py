7ccb44d58fb1c3c5932d9bf416821264c67186ee
themogulAP <themogulow@gmail.com>
2025-09-12 22:22:05 -0500

    Update MMXCMPatcher.py
    Began the patcher

--- worlds/commandmission/MMXCMPatcher.py
+++ worlds/commandmission/MMXCMPatcher.py
@@ -1,1 +1,18 @@
+import os
+import json
+import copy
+import re
+from math import ceil
+from random import choice, randint
 
+from .Item import ALL_ITEMS_TABLE, MMXCMItemData
+
+def __get_item_name(item_data, slot: int):
+    """
+    This will give us the correct in game name for each item based on item_data
+    """
+    # - Put the item name conversion logic here
+    pass
+
+def create_patch(output_data: dict, base_path: str, destination_path: str):
+    pass
