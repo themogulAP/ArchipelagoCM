3c9481695cc4444a8cb44ce5931ea27ca044792b
themogulAP <themogulow@gmail.com>
2025-09-13 01:38:09 -0500

    Update MMXCMClient.py

--- worlds/commandmission/MMXCMClient.py
+++ worlds/commandmission/MMXCMClient.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import time
 import traceback
-import dolphin_memory_engine as dme
+import dolphin_memory_engine as dolphin
 import NetUtils, Utils
 from CommonClient import server_loop
 from .MMXCMContext(CommonContext):
