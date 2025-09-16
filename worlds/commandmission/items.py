e1f1940e96d70aa6081d21547c96489721d254a0
themogulAP <themogulow@gmail.com>
2025-09-15 20:42:44 -0500

    Update items.py
    added "is fixed" to the class,

--- worlds/commandmission/items.py
+++ worlds/commandmission/items.py
@@ -13,6 +13,7 @@ class MMXCMItemData(NamedTuple):
   code: Optional[int] = None
   classification: IC
   item_id: Optional[int] = None #This is the item's unique ID
+  is_fixed: Optional[bool] = None
   update_ram_addr: Optional[list[MMXCMRamData]] = None 
 
 # This is the list for every single item we are currently randomzing in AP.
