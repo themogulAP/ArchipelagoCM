10205c68d554ca0af8101f454a1bde862e7fd695
themogulAP <themogulow@gmail.com>
2025-09-15 19:38:55 -0500

    Update MMXCMClient.py
    imported helpers py

--- worlds/commandmission/MMXCMClient.py
+++ worlds/commandmission/MMXCMClient.py
@@ -16,6 +16,7 @@ from NetUtils import NetworkItem, ClientStatus
 from worlds.commandmission.locations import LOCATION_TABLE
 from worlds.commandmission.items import ALL_ITEMS_TABLE
 from MMXCMContext import MMXCMContext
+from . import helpers
 
 # The functionality to add items, weapons, sub weapons, force metals, to our dynamic inventory. 
 # RAM addresses and the slot counts for each inventory type.
