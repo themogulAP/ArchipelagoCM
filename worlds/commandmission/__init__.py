01f760d6be51b5cd3ebdca5094b3ec71dd4794da
themogulAP <themogulow@gmail.com>
2025-09-20 16:56:21 -0500

    Update __init__.py
    fixed comment out

--- worlds/commandmission/__init__.py
+++ worlds/commandmission/__init__.py
@@ -127,15 +127,11 @@ class MMXCMWorld(World):
         state.has("Defeated Great Redips", self.player)
   
 # This will provide the slot data information upon connecting to AP! 
-"""
-def fill_slot_data(self):
-    slot_data = {
-        "rebellion_medal_count": self.options.rebellion_medal_count.value,
-        "total_locations": len(LOCATION_TABLE),
-        "encounter_rate": self.options.encounter_rate.value
-    }
-    return slot_data 
-    """
-  
-  
-    
+  def fill_slot_data(self):
+    # slot_data = {
+    #     "rebellion_medal_count": self.options.rebellion_medal_count.value,
+    #     "total_locations": len(LOCATION_TABLE),
+    #     "encounter_rate": self.options.encounter_rate.value
+    # }
+    # return slot_data
+    return {} # Temporarily returning an empty dictionary
