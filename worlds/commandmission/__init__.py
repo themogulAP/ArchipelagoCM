d2518593426642e315e0ef00fd490991babddeff
themogulAP <themogulow@gmail.com>
2025-09-20 18:58:14 -0500

    Update __init__.py
    fill slot data based on LM structure

--- worlds/commandmission/__init__.py
+++ worlds/commandmission/__init__.py
@@ -128,10 +128,8 @@ class MMXCMWorld(World):
   
 # This will provide the slot data information upon connecting to AP! 
   def fill_slot_data(self):
-    # slot_data = {
-    #     "rebellion_medal_count": self.options.rebellion_medal_count.value,
-    #     "total_locations": len(LOCATION_TABLE),
-    #     "encounter_rate": self.options.encounter_rate.value
-    # }
-    # return slot_data
-    return {} # Temporarily returning an empty dictionary
+      return {
+        "rebellion_medal_count": self.options.rebellion_medal_count.value,
+        "total_locations": len(LOCATION_TABLE),
+        "encounter_rate": self.options.encounter_rate.value
+  }
