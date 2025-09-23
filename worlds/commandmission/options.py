a270868559873d5c83758c371bb5bc6a625a054c
themogulAP <themogulow@gmail.com>
2025-09-22 21:55:22 -0500

    Update options.py
    Attempting to fix slot error

--- worlds/commandmission/options.py
+++ worlds/commandmission/options.py
@@ -2,24 +2,35 @@
 from dataclasses import dataclass
 from Options import Toggle, Range, Choice, PerGameCommonOptions
 
+# Define each option as its own class first.
+class RebellionMedalCount(Range):
+    display_name = "Rebellion Medal Count"
+    internal_name = "rebellion_medal_count"
+    default=9
+    range_start=0
+    range_end=9
+
+class EncounterRate(Choice):
+    display_name = "Encounter Rate"
+    internal_name = "encounter_rate"
+    option_off = 0
+    option_vanilla = 1
+    option_lower = 2
+    option_higher = 3
+    default = 1
+
+# The dataclass then uses these classes directly as a container.
 @dataclass
 class MMXCMOptions(PerGameCommonOptions):
     """
     MMXCMOptions is our data class that will represent the user options for
     the Mega Man X: Command Mission world in Archipelago!
     """
+    rebellion_medal_count: RebellionMedalCount
+    encounter_rate: EncounterRate
 
-    rebellion_medal_count: Range = Range(
-        display_name="Rebellion Medal Count",
-        internal_name="rebellion_medal_count",
-        default=9,
-        range_start=0,
-        range_end=9
-    )
-
-    encounter_rate: Choice = Choice(
-        display_name="Encounter Rate",
-        internal_name="encounter_rate",
-        options={"off": 0, "vanilla": 1, "lower": 2, "higher": 3},
-        default=1
-    )
+# This is the dictionary that __init__.py will import.
+options = {
+    "rebellion_medal_count": RebellionMedalCount,
+    "encounter_rate": EncounterRate
+}
