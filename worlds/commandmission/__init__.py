21818e99b07fb6f3c8813dd4986cd18b01dd3588
themogulAP <themogulow@gmail.com>
2025-09-23 01:41:41 -0500

    Update __init__.py

--- worlds/commandmission/__init__.py
+++ worlds/commandmission/__init__.py
@@ -2,7 +2,6 @@ from typing import ClassVar
 
 from BaseClasses import Region, Location, Item
 from worlds.AutoWorld import World
-from worlds.generic.Rules import set_completion_rule
 from .items import ALL_ITEMS_TABLE, FILLER_TABLE
 from .locations import LOCATION_TABLE
 from .rules import set_rules
