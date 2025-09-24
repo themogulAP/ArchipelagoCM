f79c3312c1458a65bb61321a7f60a844a946f0e5
themogulAP <themogulow@gmail.com>
2025-09-24 02:55:18 -0500

    Update __init__.py

--- worlds/commandmission/__init__.py
+++ worlds/commandmission/__init__.py
@@ -9,6 +9,7 @@ from .locations import LOCATION_TABLE
 from .rules import set_rules
 from .options import MMXCMOptions
 from .helpers import MMXCMPlayerContainer
+from .patcher import MMXCMPatcher
 import random
 import os
 
