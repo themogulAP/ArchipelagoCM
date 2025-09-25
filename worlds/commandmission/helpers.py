ca99302f7baebcda9430d4be3cceb0d7c6570218
themogulAP <themogulow@gmail.com>
2025-09-24 20:48:26 -0500

    Update helpers.py
    removed Locations from helpers py

--- worlds/commandmission/helpers.py
+++ worlds/commandmission/helpers.py
@@ -7,18 +7,6 @@ class MMXCMRamData(NamedTuple):
     pointer_offset: Optional[int] = None
     item_count: Optional[int] = None
 
-REBELLION_MEDALS_DATA = {
-    "Rebellion Medal (Lagrano Ruins)": {"address": 0x804A2109, "bit": 2},
-    "Rebellion Medal (Central Tower)": {"address": 0x804A2109, "bit": 3},
-    "Rebellion Medal (Tianna Camp)": {"address": 0x804A2109, "bit": 4},
-    "Rebellion Medal (Gaudile Laboratory)": {"address": 0x804A2109, "bit": 5},
-    "Rebellion Medal (Ulfat Factory)": {"address": 0x804A2109, "bit": 6},
-    "Rebellion Medal (Gimialla Mine)": {"address": 0x804A2109, "bit": 7},
-    "Rebellion Medal (Vanallia Desert)": {"address": 0x804A210A, "bit": 0},
-    "Rebellion Medal (Melda Ore Plant)": {"address": 0x804A210A, "bit": 1},
-    "Rebellion Medal (Grave Ruins Base)": {"address": 0x804A210A, "bit": 2},
-}
-
 ACCESS_CODES_DATA = {
     "Lagrano Ruins Access Code": {"address": 0x804A2108, "bit": 0},
     "Central Tower Access Code": {"address": 0x804A2108, "bit": 1},
