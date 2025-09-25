5af91fa21215a523c07fdaf37c2093b7d40636d8
themogulAP <themogulow@gmail.com>
2025-09-25 15:32:00 -0500

    Update helpers.py
    added a collection of messages for connecting to the server

--- worlds/commandmission/helpers.py
+++ worlds/commandmission/helpers.py
@@ -1,4 +1,20 @@
-from typing import NamedTuple, Optional, Any
+from typing import NamedTuple, Optional
+
+""" Collection of commonly used constants for MMX Command Mission. """
+
+CLIENT_VERSION = "V0.5.7"
+CLIENT_NAME = "Mega Man X Command Mission Client"
+
+AP_LOGGER_NAME = "Client"
+AP_WORLD_VERSION_NAME = "APWorldVersion"
+
+# All the dolphin connection messages used in the client
+CONNECTION_REFUSED_STATUS = "Detected a non-randomized ROM for MMXCM. Please close and load a different one. Retrying in 5 seconds..."
+CONNECTION_LOST_STATUS = "Dolphin connection was lost. Please restart your emulator and make sure MMXCM is running."
+NO_SLOT_NAME_STATUS = "No slot name was detected. Ensure a randomized ROM is loaded. Retrying in 5 seconds..."
+CONNECTION_VERIFY_SERVER = "Dolphin was confirmed to be opened and ready, Connect to the server when ready..."
+CONNECTION_INITIAL_STATUS = "Dolphin emulator was not detected to be running. Retrying in 5 seconds..."
+CONNECTION_CONNECTED_STATUS = "Dolphin is connected, AP is connected, have fun on your MMXCM playthrough!"
 
 class MMXCMRamData(NamedTuple):
     ram_addr: Optional[int] = None
