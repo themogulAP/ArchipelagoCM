24392265d1965792bafdcaf52f96c477b2be37fe
SomeJakeGuy <somejakeguy@gmail.com>
2025-10-17 19:09:53 -0400

    Fixed init's indent and changed item_data.item_count

--- worlds/commandmission/__init__.py
+++ worlds/commandmission/__init__.py
@@ -14,191 +14,199 @@ from worlds.LauncherComponents import Component, SuffixIdentifier, Type, compone
 import random
 import os
 
+
 def run_client(*args):
     from .MMXCMClient import sync_main  # lazy import
     launch_subprocess(sync_main, name="MMMXCMClient", args=args)
 
+
 # Adds the launcher for our component and our client logo.
 components.append(
     Component("MMXCM Client", func=run_client, component_type=Type.CLIENT,
-        file_identifier=SuffixIdentifier(".apmmxcm")))
+              file_identifier=SuffixIdentifier(".apmmxcm")))
 
-#Define the MMX Command Mission Class:
+
+# Define the MMX Command Mission Class:
 class MMXCMWorld(World):
-  """
-  Mega Man X: Command Mission is a turn-based RPG set in the Mega Man X universe.
-  """
+    """
+    Mega Man X: Command Mission is a turn-based RPG set in the Mega Man X universe.
+    """
 
-# Describe the name of the game that will appear on AP Client. 
-# Link options from the options py to here, and then set player options.
-# Topology is for randomize maps, not needed for current implementation of static maps. 
-# Data Version is the current version. 
-  game: ClassVar[str] = "Mega Man X Command Mission"
-  options_dataclass = MMXCMOptions
-  options: MMXCMOptions
-  topology_present: ClassVar[bool] = False
-  data_version: ClassVar[int] = 1
-  settings: MMXCMSettings
+    # Describe the name of the game that will appear on AP Client.
+    # Link options from the options py to here, and then set player options.
+    # Topology is for randomize maps, not needed for current implementation of static maps.
+    # Data Version is the current version.
+    game: ClassVar[str] = "Mega Man X Command Mission"
+    options_dataclass = MMXCMOptions
+    options: MMXCMOptions
+    topology_present: ClassVar[bool] = False
+    data_version: ClassVar[int] = 1
+    settings: MMXCMSettings
 
-  # This will tell AP what our group actually is for Rebellion Medals.
-  item_name_groups: ClassVar[dict[str, frozenset[str]]] = {
+    # This will tell AP what our group actually is for Rebellion Medals.
+    item_name_groups: ClassVar[dict[str, frozenset[str]]] = {
         REBELLION_MEDALS_GROUP.name: REBELLION_MEDALS_GROUP.item_names
     }
 
-#Create the dictionaries that will map every item and every location for our AP! 
-  item_name_to_id: ClassVar[dict[str, int]] = {name: data.code for name, data in ALL_ITEMS_TABLE.items()}
-  location_name_to_id: ClassVar[dict[str, int]] = {name: data.code for name, data in LOCATION_TABLE.items()}
+    # Create the dictionaries that will map every item and every location for our AP!
+    item_name_to_id: ClassVar[dict[str, int]] = {name: data.code for name, data in ALL_ITEMS_TABLE.items()}
+    location_name_to_id: ClassVar[dict[str, int]] = {name: data.code for name, data in LOCATION_TABLE.items()}
 
-#This is the very first piece of code that runs when a new MMX CM World will be created! 
-  #It will then map all of our items and  to their respective Strings to IDs that we put in items and  py. 
-  def __init__(self, *args, **kwargs):
-    super(MMXCMWorld, self).__init__(*args, **kwargs)
+    # This is the very first piece of code that runs when a new MMX CM World will be created!
+    # It will then map all of our items and  to their respective Strings to IDs that we put in items and  py.
+    def __init__(self, *args, **kwargs):
+        super(MMXCMWorld, self).__init__(*args, **kwargs)
 
-# This places any logic we need to before the generation process.
-  def generate_early(self):
-   pass
+    # This places any logic we need to before the generation process.
+    def generate_early(self):
+        pass
 
-# This will build the entire map for our randomized AP! 
-  def create_regions(self):
-    # This will serve as a Master Dictionary for our loops, describing the codes needed for the same area.
-    region_data = {
-      "Lagrano Ruins": "Lagrano Ruins Access Code",
-      "Central Tower Boss Arena": "Central Tower Access Code",
-      "Tianna Camp": "Tianna Camp Access Code",
-      "Gaudile Laboratory": "Gaudile Laboratory Access Code",
-      "Ulfat Factory": "Ulfat Factory Access Code",
-      "Gimialla Mine": "Gimialla Mine Access Code",
-      "Vanallia Desert": "Vanallia Desert Access Code",
-      "Melda Ore Plant": "Melda Ore Plant Access Code",
-      "Grave Ruins Base": "Grave Ruins Base Access Code",
-      "Far East HQ": "Far East HQ Access Code"
-    }
+    # This will build the entire map for our randomized AP!
+    def create_regions(self):
+        # This will serve as a Master Dictionary for our loops, describing the codes needed for the same area.
+        region_data = {
+            "Lagrano Ruins": "Lagrano Ruins Access Code",
+            "Central Tower Boss Arena": "Central Tower Access Code",
+            "Tianna Camp": "Tianna Camp Access Code",
+            "Gaudile Laboratory": "Gaudile Laboratory Access Code",
+            "Ulfat Factory": "Ulfat Factory Access Code",
+            "Gimialla Mine": "Gimialla Mine Access Code",
+            "Vanallia Desert": "Vanallia Desert Access Code",
+            "Melda Ore Plant": "Melda Ore Plant Access Code",
+            "Grave Ruins Base": "Grave Ruins Base Access Code",
+            "Far East HQ": "Far East HQ Access Code"
+        }
 
-    menu_region = Region("Menu", self.player, self.multiworld)
-    self.multiworld.regions.append(menu_region)
+        menu_region = Region("Menu", self.player, self.multiworld)
+        self.multiworld.regions.append(menu_region)
 
-    #Create our Central Tower main hub and full verision (when code is received)! 
-    central_tower_hub_region = Region("Central Tower Hub", self.player, self.multiworld)
-    central_tower_full_region = Region("Central Tower Full", self.player, self.multiworld)
-    central_tower_boss_arena_region = Region("Central Tower Boss Arena", self.player, self.multiworld)
-    far_east_hq_region = Region("Far East HQ", self.player, self.multiworld)
-    self.multiworld.regions.append(central_tower_hub_region)
-    self.multiworld.regions.append(central_tower_full_region)
+        # Create our Central Tower main hub and full verision (when code is received)!
+        central_tower_hub_region = Region("Central Tower Hub", self.player, self.multiworld)
+        central_tower_full_region = Region("Central Tower Full", self.player, self.multiworld)
+        central_tower_boss_arena_region = Region("Central Tower Boss Arena", self.player, self.multiworld)
+        far_east_hq_region = Region("Far East HQ", self.player, self.multiworld)
+        self.multiworld.regions.append(central_tower_hub_region)
+        self.multiworld.regions.append(central_tower_full_region)
 
-    menu_region.connect(central_tower_hub_region)
+        menu_region.connect(central_tower_hub_region)
 
-    #Connect the Regions here.
-    central_tower_hub_region.connect(
-      central_tower_full_region,
-      rule=lambda state: state.has("Central Tower Access Code", self.player)
-    )
+        # Connect the Regions here.
+        central_tower_hub_region.connect(
+            central_tower_full_region,
+            rule=lambda state: state.has("Central Tower Access Code", self.player)
+        )
 
-    central_tower_full_region.connect(
-        central_tower_boss_arena_region
-    )
-    central_tower_boss_arena_region.connect(
-        far_east_hq_region,
-        rule=lambda state: state.has_group("Rebellion Medals", self.player, 9) and
-        state.has("Far East HQ Access Code", self.player)
-    )
+        central_tower_full_region.connect(
+            central_tower_boss_arena_region
+        )
+        central_tower_boss_arena_region.connect(
+            far_east_hq_region,
+            rule=lambda state: state.has_group("Rebellion Medals", self.player, 9) and
+                               state.has("Far East HQ Access Code", self.player)
+        )
 
-    # Create all the other regions and connect them to the Central Tower hub!
-    for region_name, access_code in region_data.items():
-      new_region = Region(region_name, self.player, self.multiworld)
-      self.multiworld.regions.append(new_region)
+        # Create all the other regions and connect them to the Central Tower hub!
+        for region_name, access_code in region_data.items():
+            new_region = Region(region_name, self.player, self.multiworld)
+            self.multiworld.regions.append(new_region)
 
-      central_tower_hub_region.connect(
-        new_region,
-        rule=lambda state, code=access_code: state.has(code, self.player)
-      )
+            central_tower_hub_region.connect(
+                new_region,
+                rule=lambda state, code=access_code: state.has(code, self.player)
+            )
 
-    # Add Every location from our  py to their regions! 
-    for location_name, location_data in LOCATION_TABLE.items():
-      region=self.multiworld.get_region(location_data.parent_region, self.player)
-      location = Location(
-        self.player,
-        location_name,
-        location_data.code,
-        region,
-      )
-      region.locations.append(location)
+        # Add Every location from our  py to their regions!
+        for location_name, location_data in LOCATION_TABLE.items():
+            region = self.multiworld.get_region(location_data.parent_region, self.player)
+            location = Location(
+                self.player,
+                location_name,
+                location_data.code,
+                region,
+            )
+            region.locations.append(location)
 
-  def generate_basic(self):
-      self.multiworld.get_location("Defeated Great Redips", self.player).place_locked_item(
-          self.create_item("Defeated Great Redips")
-      )
+    def generate_basic(self):
+        self.multiworld.get_location("Defeated Great Redips", self.player).place_locked_item(
+            self.create_item("Defeated Great Redips")
+        )
 
-      # This will be for setting our rebellion medals STATICALLY to their spots.
-      rebellion_medal_names = [
-          "Rebellion Medal 1", "Rebellion Medal 2", "Rebellion Medal 3",
-          "Rebellion Medal 4", "Rebellion Medal 5", "Rebellion Medal 6",
-          "Rebellion Medal 7", "Rebellion Medal 8", "Rebellion Medal 9"
-      ]
-      for medal_name in rebellion_medal_names:
-          self.multiworld.get_location(medal_name, self.player).place_locked_item(
-              self.create_item(medal_name)
-          )
+        # This will be for setting our rebellion medals STATICALLY to their spots.
+        rebellion_medal_names = [
+            "Rebellion Medal 1", "Rebellion Medal 2", "Rebellion Medal 3",
+            "Rebellion Medal 4", "Rebellion Medal 5", "Rebellion Medal 6",
+            "Rebellion Medal 7", "Rebellion Medal 8", "Rebellion Medal 9"
+        ]
+        for medal_name in rebellion_medal_names:
+            self.multiworld.get_location(medal_name, self.player).place_locked_item(
+                self.create_item(medal_name)
+            )
 
-# This will build the entire item pool for our randomized AP! 
-  def create_items(self):
-      item_pool = []
-      for item_name, item_data in ALL_ITEMS_TABLE.items():
+    # This will build the entire item pool for our randomized AP!
+    def create_items(self):
+        item_pool = []
+        for item_name, item_data in ALL_ITEMS_TABLE.items():
 
-          # Exclude the Event item Great Redips and statically place it on the location. 
-          if item_name == "Defeated Great Redips":
-                continue 
-        
-          if "Rebellion Medal" in item_name:
+            # Exclude the Event item Great Redips and statically place it on the location.
+            if item_name == "Defeated Great Redips":
                 continue
 
-          quantity = getattr(item_data, 'item_count', 1)
+            if "Rebellion Medal" in item_name:
+                continue
 
-          for _ in range(quantity):
-            item_pool.append(self.create_item(item_name))
+            # Create the minimum amount of items for the given item in the list
+            for _ in range(item_data.item_count):
+                item_pool.append(self.create_item(item_name))
 
-      self.multiworld.itempool.extend(item_pool)
+        self.multiworld.itempool.extend(item_pool)
 
-    # This adds our filler items, and will calculate the number to add. 
-      locations_count = len(self.multiworld.get_unfilled_locations(self.player))
-      items_in_pool = len(self.multiworld.itempool)
-      filler_needed = locations_count - items_in_pool
+        # This adds our filler items, and will calculate the number to add.
+        locations_count = len(self.multiworld.get_unfilled_locations(self.player))
+        items_in_pool = len(self.multiworld.itempool)
+        filler_needed = locations_count - items_in_pool
 
-#Randomly selects the filler items to add into the pool.
-      filler_items_to_add = random.choices(list(FILLER_TABLE.keys()), k=filler_needed)
+        # Randomly selects the filler items to add into the pool.
+        filler_items_to_add = random.choices(list(FILLER_TABLE.keys()), k=filler_needed)
 
-      for filler_item_name in filler_items_to_add:
-          self.multiworld.itempool.append(self.create_item(filler_item_name))
+        for filler_item_name in filler_items_to_add:
+            self.multiworld.itempool.append(self.create_item(filler_item_name))
 
-      # It is the helper that the create_items method calls.
-  def create_item(self, name: str) -> Item:
-    item_data = ALL_ITEMS_TABLE[name]
-    return Item(name, item_data.classification, item_data.code, self.player)
+        # It is the helper that the create_items method calls.
 
-# This will apply all the logic that we described in rules py! 
-  def set_rules(self):
-      set_rules(self)
 
-  # Creates the dictionary for all locations in output data, sets the path and gives us a patch to downlaod! 
-  def generate_output(self, output_directory: str, **kwargs):
-      # Outputs the plando details to our expected output file
-      # Create the output path based on the current player + expected patch file ending.
-      patch_path = os.path.join(output_directory, f"{self.multiworld.get_out_file_name_base(self.player)}"
-                                                  f"{MMXCMPlayerContainer.patch_file_ending}")
-      # Create a zip (container) that will contain all the necessary output files for us to use during patching.
-      dict_output_data: dict = {
-          "Seed": self.multiworld.seed,
-          "Slot": self.player,
-          "Name": self.player_name,
-          "Locations": {},
-          "APWorldVersion": CLIENT_VERSION
-      }
+    def create_item(self, name: str) -> Item:
+        item_data = ALL_ITEMS_TABLE[name]
+        return Item(name, item_data.classification, item_data.code, self.player)
+
+
+    # This will apply all the logic that we described in rules py!
+    def set_rules(self):
+        set_rules(self)
+
+
+    # Creates the dictionary for all locations in output data, sets the path and gives us a patch to downlaod!
+    def generate_output(self, output_directory: str, **kwargs):
+        # Outputs the plando details to our expected output file
+        # Create the output path based on the current player + expected patch file ending.
+        patch_path = os.path.join(output_directory, f"{self.multiworld.get_out_file_name_base(self.player)}"
+                                                    f"{MMXCMPlayerContainer.patch_file_ending}")
+        # Create a zip (container) that will contain all the necessary output files for us to use during patching.
+        dict_output_data: dict = {
+            "Seed": self.multiworld.seed,
+            "Slot": self.player,
+            "Name": self.player_name,
+            "Locations": {},
+            "APWorldVersion": CLIENT_VERSION
+        }
 
         # Creates the zip that will hold all necessary output files.
-      mmxcm_container = MMXCMPlayerContainer(dict_output_data, patch_path, self.multiworld.player_name[self.player], self.player)
-      # Write the expected output zip container to the Generated Seed folder.
-      mmxcm_container.write()
+        mmxcm_container = MMXCMPlayerContainer(dict_output_data, patch_path, self.multiworld.player_name[self.player],
+                                               self.player)
+        # Write the expected output zip container to the Generated Seed folder.
+        mmxcm_container.write()
 
-  def fill_slot_data(self):
+
+    def fill_slot_data(self):
         """This will provide the slot data information upon connecting to AP."""
         return {
             "rebellion_medal_count": self.options.rebellion_medal_count.value,
