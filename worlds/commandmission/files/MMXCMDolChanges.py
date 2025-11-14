08827ddd321a3ea999e77d11873ee4078fd76daa
TheMogulTTV <themogulow@gmail.com>
2025-11-14 00:10:42 -0600

    Added the Chief R fix soft lock thingy

--- worlds/commandmission/files/MMXCMDolChanges.py
+++ worlds/commandmission/files/MMXCMDolChanges.py
@@ -71,39 +71,51 @@ CODE_PATCHES = [
         "data": [0x98, 0x64, 0x00, 0x5D] # 98 = Store BYTE in PPC.
     },
     {
-        # Sets the Preon Battle 2E in Command Room to Finished -
+        # Sets the Preon Battle 2C in Chief R hostage Room to Finished -
         # RAM: 8000d910
         "address": 0x0A910,
+        "data": [0x38, 0x60, 0x00, 0x24]
+    },
+    {
+        # STORES the Preon Battle 2C in Chief R hostage Room to Finished -
+        # RAM: 8000d914
+        "address": 0x0A914,
+        "data": [0x98, 0x64, 0x00, 0x61]
+    },
+    {
+        # Sets the Preon Battle 2E in Command Room to Finished -
+        # RAM: 8000d918
+        "address": 0x0A918,
         "data": [0x38, 0x60, 0x00, 0x04]
     },
     {
         # STORE the Preon Battle 2E in Command Room to Finished -
-        # RAM: 8000d914
-        "address": 0x0A914,
+        # RAM: 8000d91C
+        "address": 0x0A91C,
         "data": [0x98, 0x64, 0x00, 0x62]
     },
     {
         # Sets BYTE FOR Arcade, Jango's, and Bed Door
-        # RAM: 8000d918
-        "address": 0x0A918,
+        # RAM: 8000d920
+        "address": 0x0A920,
         "data": [0x38, 0x60, 0x00, 0x19]
     },
     {
         # Stores BYTE FOR Arcade, Jango's, and Bed Door
-        # RAM: 8000d91C
-        "address": 0x0A91C,
+        # RAM: 8000d924
+        "address": 0x0A924,
         "data": [0x98, 0x64, 0x00, 0x63]
     },
     {
         # sets every other flag back to zero
-        # RAM: 8000d920
-        "address": 0x0A920,
+        # RAM: 8000d928
+        "address": 0x0A928,
         "data": [0x38, 0x60, 0x00, 0x00]
     },
     {
         # Stores every other flag to zero
         # RAM: 8000d92C
-        "address": 0x0A924,
+        "address": 0x0A92C,
         "data": [0x90, 0x64, 0x00, 0x74]
     },
     {
